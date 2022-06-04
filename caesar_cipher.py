abf5ac33e61eaea651d1d2bd3c7758deed89495f
Javiermolinah <ing.molina.h@gmail.com>
2022-06-04 18:14:11 -0500

    Caesar Cipher Implemented

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,19 @@
 
+from typing import Callable, Dict
+
+
 def encrypt(plaintext: str, shift: int) -> str:
+    encrypt: Dict[str, Callable[[str, int], str]] = {
+        'uppercase': lambda char, shift:
+            chr((ord(char) + shift - 65) % 26 + 65),
+        'lowercase': lambda char, shift:
+            chr((ord(char) + shift - 97) % 26 + 97)
+    }
+
     result = ''
+
+    for char in plaintext:
+        key = 'uppercase' if char.isupper() else 'lowercase'
+        result += encrypt[key](char, shift)
 
     return result
