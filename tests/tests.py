20b631870e4b390dc4c42c4802563f2c63ff9340
Javiermolinah <ing.molina.h@gmail.com>
2022-06-04 18:04:27 -0500

    Initial tests defined

---
+++ tests/tests.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+
+class CaesarTest(TestCase):
+
+    def test_hello_phrase(self):
+        pass
+
+    def test_a_list_of_words(self):
+        pass
+
+    def test_large_phrase(self):
+        pass
+
+    def test_without_command_line_argument_error(self):
+        pass
+
+    def test_more_than_one_command_line_arguments_error(self):
+        pass
+
+    def test_negative_command_line_argument_error(self):
+        pass
