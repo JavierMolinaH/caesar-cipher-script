792ab2815c8c70a04b723945c66efad52f8d0ece
Javiermolinah <ing.molina.h@gmail.com>
2022-06-04 19:30:10 -0500

    Cipher only alpha chars

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,18 +2,17 @@
 from typing import Callable, Dict
 
 
+encrypt_char: Dict[str, Callable[[str, int], str]] = {
+    'uppercase': lambda char, shift:
+        chr((ord(char) + shift - 65) % 26 + 65),
+    'lowercase': lambda char, shift:
+        chr((ord(char) + shift - 97) % 26 + 97)
+}
+
+
 def encrypt(plaintext: str, shift: int) -> str:
-    encrypt: Dict[str, Callable[[str, int], str]] = {
-        'uppercase': lambda char, shift:
-            chr((ord(char) + shift - 65) % 26 + 65),
-        'lowercase': lambda char, shift:
-            chr((ord(char) + shift - 97) % 26 + 97)
-    }
-
     result = ''
-
     for char in plaintext:
         key = 'uppercase' if char.isupper() else 'lowercase'
-        result += encrypt[key](char, shift)
-
+        result += encrypt_char[key](char, shift) if char.isalpha() else char
     return result
